24427255d4d1d33dabd9587c66abb008b4b85082
Lachlan Orton <56850750+lachlanorton@users.noreply.github.com>
2024-09-17 19:59:59 +1000

    Day 5 "Creative Day" ft. sequential lights

---
+++ Day5/Day5.ino
@@ -0,0 +1,83 @@
+//#include "Arduino.h"
+
+//#define CABIN_LIGHT_PIN 12
+//#define CABIN_LIGHT_SWITCH_PIN 2
+const byte RED_LIGHT_PIN = 10;
+const byte GREEN_LIGHT_PIN = 11;
+const byte BLUE_LIGHT_PIN = 12;
+
+const byte RED_LIGHT_SWITCH_PIN = 2;
+const byte GREEN_LIGHT_SWITCH_PIN = 3;
+const byte BLUE_LIGHT_SWITCH_PIN = 4;
+
+
+void setup() {
+  // Set Cabin Light Pin as an Output pin
+  pinMode(RED_LIGHT_PIN, OUTPUT);
+  pinMode(GREEN_LIGHT_PIN, OUTPUT);
+  pinMode(BLUE_LIGHT_PIN, OUTPUT);
+
+  pinMode(RED_LIGHT_SWITCH_PIN, INPUT);
+  pinMode(GREEN_LIGHT_SWITCH_PIN, INPUT);
+  pinMode(BLUE_LIGHT_SWITCH_PIN, INPUT);
+}
+
+void loop() {
+  if(digitalRead(RED_LIGHT_SWITCH_PIN) == HIGH)
+  {
+    digitalWrite(BLUE_LIGHT_PIN, LOW);
+    digitalWrite(RED_LIGHT_PIN, HIGH);
+    delay(1000);
+    digitalWrite(RED_LIGHT_PIN, LOW);
+    digitalWrite(GREEN_LIGHT_PIN, HIGH);
+    delay(1000);
+    digitalWrite(GREEN_LIGHT_PIN, LOW);
+    digitalWrite(BLUE_LIGHT_PIN, HIGH);
+    delay(1000);
+
+  }
+  else
+  {
+    digitalWrite(RED_LIGHT_PIN, LOW);
+    digitalWrite(GREEN_LIGHT_PIN, LOW);
+    digitalWrite(BLUE_LIGHT_PIN, LOW);
+  }
+
+  if(digitalRead(GREEN_LIGHT_SWITCH_PIN) == HIGH)
+  {
+    digitalWrite(RED_LIGHT_PIN, LOW);
+    digitalWrite(BLUE_LIGHT_PIN, LOW);
+    digitalWrite(GREEN_LIGHT_PIN, HIGH);
+    delay(500);
+    digitalWrite(RED_LIGHT_PIN, HIGH);
+    digitalWrite(BLUE_LIGHT_PIN, HIGH);
+    digitalWrite(GREEN_LIGHT_PIN, LOW);
+    delay(500);
+  }
+  else
+  {
+    digitalWrite(GREEN_LIGHT_PIN, LOW);
+    digitalWrite(RED_LIGHT_PIN, LOW);
+    digitalWrite(BLUE_LIGHT_PIN, LOW);
+  }
+
+  if(digitalRead(BLUE_LIGHT_SWITCH_PIN) == HIGH)
+  {
+    digitalWrite(RED_LIGHT_PIN, LOW);
+    digitalWrite(BLUE_LIGHT_PIN, HIGH);
+    delay(1000);
+    digitalWrite(BLUE_LIGHT_PIN, LOW);
+    digitalWrite(GREEN_LIGHT_PIN, HIGH);
+    delay(1000);
+    digitalWrite(GREEN_LIGHT_PIN, LOW);
+    digitalWrite(RED_LIGHT_PIN, HIGH);
+    delay(1000);
+  }
+  else
+  {
+    digitalWrite(RED_LIGHT_PIN, LOW);
+    digitalWrite(GREEN_LIGHT_PIN, LOW);
+    digitalWrite(BLUE_LIGHT_PIN, LOW);
+  }
+}
+
